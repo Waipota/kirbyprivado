5db9f5da6a4dc6c44b7be32093b91330df187b4a
waipota46 <santiagontiveros.lascolinas@gmail.com>
2024-11-10 00:46:14 -0400

    Meti los folders con los archivos

---
+++ src/ui/MenuButton.java
@@ -0,0 +1,83 @@
+package ui;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import gamestates.GameState;
+import utilz.LoadSave;
+import static utilz.Constants.UI.Buttons.*;
+public class MenuButton {
+  private int xpos, ypos, row, index;
+  private int xOffsetcenter = B_WIDHT / 2;
+  private GameState state;
+  private BufferedImage[] imgs;
+  private boolean mouseOver, mousePressed;
+  private Rectangle bounds;
+  public MenuButton(int xpos, int ypos, int row, GameState state) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.row = row;
+    this.state = state;
+    loadImgs();
+    initBounds();
+  }
+
+  private void initBounds(){
+    bounds = new Rectangle(xpos - xOffsetcenter, ypos, B_WIDHT, B_HEIGHT);
+
+  }
+
+
+  private void loadImgs() {
+    imgs = new BufferedImage[3];
+    BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.MENU_BUTTON);
+    for (int i = 0 ; i < imgs.length; i ++){
+      imgs[i] = temp.getSubimage(i * B_WIDHT_DEFAULT, row * B_HEIGHT_DEFAULT, B_WIDHT_DEFAULT, B_HEIGHT_DEFAULT);
+    }
+  }
+
+  public void draw(Graphics g) {
+    g.drawImage(imgs[index], xpos - xOffsetcenter, ypos , B_WIDHT, B_HEIGHT, null);
+  }
+
+  public void update(){
+
+    index = 0;
+    if (mouseOver){
+      index = 1;
+    }
+    if (mousePressed){
+      index = 2;
+    }
+
+
+  }
+
+  public boolean getMouseOver(){
+    return mouseOver;
+  }
+
+  public boolean getMousePressed(){
+    return mousePressed;
+  }
+
+  public void setMouseOver(boolean mouseOver){
+    this.mouseOver = mouseOver;
+  }
+
+  public void setMousePressed(boolean mousePressed){
+    this.mousePressed = mousePressed;
+  }
+
+  public void applyGamestate(){
+    GameState.state = state;
+  }
+
+  public void resetBools(){
+    mouseOver = false;
+    mousePressed = false; 
+  }
+
+
+}
---
+++ src/utilz/LoadSave.java
@@ -0,0 +1,53 @@
+package utilz;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.Buffer;
+
+import javax.imageio.ImageIO;
+
+import main.Game;
+
+public class LoadSave {
+
+	public static final String PLAYER_ATLAS = "player_sprites.png";
+	public static final String LEVEL_ATLAS = "outside_sprites.png";
+	public static final String LEVEL_ONE_DATA = "level_one_data.png";
+  public static final String MENU_BUTTON = "button_atlas.png";
+
+
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+    BufferedImage imagen = null;
+  try {
+    File file = new File("../Resources/" + fileName);
+    imagen = ImageIO.read(file);
+    if (imagen == null) {
+      System.out.println("Error: No se pudo cargar la imagen ");
+      return null;
+    }
+  } catch (IOException e) {
+    e.printStackTrace();
+  }
+
+  return imagen;
+	}
+
+	public static int[][] GetLevelData() {
+		int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+
+		for (int j = 0; j < img.getHeight(); j++)
+			for (int i = 0; i < img.getWidth(); i++) {
+				Color color = new Color(img.getRGB(i, j));
+				int value = color.getRed();
+				if (value >= 48)
+					value = 0;
+				lvlData[j][i] = value;
+			}
+		return lvlData;
+
+	}
+}
